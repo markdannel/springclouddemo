facf8a56cda15325f320a89f9d99ac59dc8def4f
fish <wistion@foxmail.com>
2019-12-09 17:51:07 +0800

    add eureka-consumer demo

---
+++ eureka-consumer/src/test/java/com/marksim/spring/eurekaserver/eurekaconsumer/EurekaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marksim.spring.eurekaserver.eurekaconsumer;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
