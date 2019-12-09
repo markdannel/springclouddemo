f0257dea35736d5c3987539a373c667746af9775
fish <wistion@foxmail.com>
2019-12-09 18:28:50 +0800

    add eureka-consumer-ribbon demo

---
+++ eureka-consumer-ribbon/src/test/java/com/marksim/spring/eurekaserver/eurekaconsumerribbon/EurekaConsumerRibbonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marksim.spring.eurekaserver.eurekaconsumerribbon;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaConsumerRibbonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
