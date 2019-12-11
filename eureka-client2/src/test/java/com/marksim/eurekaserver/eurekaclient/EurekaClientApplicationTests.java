baafe674e035df0b081a32df90a70eec3c69ab28
markdannel <wistion@foxmail.com>
2019-12-11 23:49:23 +0800

    add config-client

---
+++ eureka-client2/src/test/java/com/marksim/eurekaserver/eurekaclient/EurekaClientApplicationTests.java
@@ -0,0 +1,14 @@
+package com.marksim.eurekaserver.eurekaclient;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/marksim/spring/eurekaserver/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marksim.spring.eurekaserver.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
