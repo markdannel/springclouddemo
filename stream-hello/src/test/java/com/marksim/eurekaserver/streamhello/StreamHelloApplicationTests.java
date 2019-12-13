c04e535c98e2895b15970524ddd5f47420a0230d
fish <wistion@foxmail.com>
2019-12-13 17:30:07 +0800

    add stream-demo

---
+++ stream-hello/src/test/java/com/marksim/eurekaserver/streamhello/StreamHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marksim.eurekaserver.streamhello;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
